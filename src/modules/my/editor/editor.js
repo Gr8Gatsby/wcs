3342b208984e03d8c02c5fa48b834ba069a7eae2
gr8gatsby <kevinhill@live.com>
2021-06-13 22:44:37 -0700

    updating editor

--- src/modules/my/editor/editor.js
+++ src/modules/my/editor/editor.js
@@ -1,12 +1,33 @@
-import {LightningElement, api, track} from 'lwc';
+import {LightningElement, track} from 'lwc';
 
 export default class Editor extends LightningElement {
+  
+  @track editorElement;
+  
   handleInput (event) {
-    let text = event.target.value;
-    console.log (text);
-    let match = /\r|\n/.exec (text);
-    if (match) {
-      console.log ('newline!', match.index);
+    console.log(event.target.selectionStart,event.target.selectionEnd);
+  }
+
+  insertMarkdown(symbol){
+    let nSelStart = this.editorElement.selectionStart
+    let nSelEnd = this.editorElement.selectionEnd
+    let sOldText = this.editorElement.value;
+    let update = '';
+    if(sOldText.substring(nSelEnd,nSelEnd-1) === ' ' && sOldText.substring(nSelEnd,nSelEnd-2) !== ' ' ){
+      update = sOldText.substring(0,nSelStart) + symbol + sOldText.substring(nSelStart,nSelEnd -1) + symbol + ' ' + sOldText.substring(nSelEnd);
+    } else {
+      update = sOldText.substring(0,nSelStart) + symbol + sOldText.substring(nSelStart,nSelEnd) + symbol + sOldText.substring(nSelEnd);
     }
+    this.editorElement.value = update;
+    // sOldText.substring(0, nSelStart) + (bDouble ? sStartTag + sOldText.substring(nSelStart, nSelEnd) + sEndTag : sStartTag) + sOldText.substring(nSelEnd);
+  }
+
+  addBold(){
+    this.insertMarkdown("*");
+  }
+
+  renderedCallback(){
+    this.editorElement = this.template.querySelector('textarea');
+    console.log(this.editorElement);
   }
 }
